a74ed5165e0642cdcb2600385c8477392632cc9e
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2025-09-28 22:27:23 +0700

    [Lab: Laravel Framework] Section 6 - Security | [Part 2. User Profile Feature]

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -45,6 +45,11 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.reset-password', ['request' => $request]);
         });
 
+        Fortify::verifyEmailView(function () {
+            return view('auth.verify-email');
+        });
+
+
         Fortify::createUsersUsing(callback: CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
