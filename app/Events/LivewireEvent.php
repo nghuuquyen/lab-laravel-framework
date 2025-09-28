9059330915b8d65909ddbf80610e5218bd8287e1
Quyen Nguyen Huu <nghuuquyen@gmail.com>
2025-09-28 16:17:44 +0700

    [Lab: Laravel Framework] Section 4 - Build the User Interface | [Part 2. Build the Add to Cart]

---
+++ app/Events/LivewireEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Events;
+
+class LivewireEvent {
+
+    public const CART_UPDATED_EVENT = 'CART_UPDATED_EVENT';
+
+    public const USER_SELECT_PRODUCT = 'USER_SELECT_PRODUCT_EVENT';
+
+}
